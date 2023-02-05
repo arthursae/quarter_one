02c7bec12114cbc1060b9a9626636198b2990b82
Artur_A <arthursae@mail.ru>
2023-02-06 00:21:35 +0500

    Empty lines removed

--- task/Program.cs
+++ task/Program.cs
@@ -1,14 +1,4 @@
-﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых меньше либо равна 3 символа.
-// Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма.
-// При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
-//
-// Примеры:
-//
-// ["hello", "2", "world", ":-)"] -> ["2", ":-)"]
-// ["1234", "1567", "-2", "computer science"] -> ["-2"]
-// ["Russia", "Denmark", "Kazan"] -> []
-
-using System.Text.RegularExpressions;
+﻿using System.Text.RegularExpressions;
 
 void OutputBeforeAndAfterResult(string[] userData, string[] filteredArray)
 {
@@ -28,7 +18,6 @@ int CountNumberOfItemsBelowCharsLimit(int charsLimit, string[] userData)
         if (item.Length <= charsLimit)
             counter++;
     }
-
     return counter;
 }
 
@@ -49,7 +38,6 @@ string[] FilterUserDataArray(int charsLimit, string[] userData)
                 i++;
             }
         }
-
         return filteredArray;
     }
     else
